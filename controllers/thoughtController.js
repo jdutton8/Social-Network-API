739c0131986258ab0a2c58b9bfe0523b3bbe6f6d
jdutton8 <jennydutton7@gmail.com>
2023-08-06 12:22:16 -0500

    added thoughts and reaction modules

--- controllers/thoughtController.js
+++ controllers/thoughtController.js
@@ -4,7 +4,7 @@ module.exports = {
   // Get all thoughts
   async getThoughts(req, res) {
     try {
-      const thoughts = await Thought.find();
+      const thoughts = await Thought.find().populate('reactions');
       res.json(thoughts);
     } catch (err) {
       res.status(500).json(err);
--- controllers/userController.js
+++ controllers/userController.js
@@ -1,4 +1,4 @@
-// ObjectId() method for converting studentId string into an ObjectId for querying database
+// ObjectId() method for converting userId string into an ObjectId for querying database
 const { ObjectId } = require('mongoose').Types;
 const { User, Thought } = require('../models');
 
